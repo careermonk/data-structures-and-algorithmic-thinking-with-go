c41334209b1fe10d58edee5aff615533e3d2853f
CareerMonk <careermonk@users.noreply.github.com>
2020-07-12 09:15:40 +0530

    Create factorial_recursive.go

---
+++ chapter02-recursion-and-backtracking/factorial_recursive.go
@@ -0,0 +1,25 @@
+/*
+# Copyright (c) July 12, 2020 CareerMonk Publications and others.
+# E-Mail           		: info@careermonk.com 
+# Creation Date    		: 2020-07-12 06:15:46 
+# Last modification		: 2020-07-12 
+#               by		: Narasimha Karumanchi 
+# Book Title			: Data Structures And Algorithmic Thinking With Go
+# Warranty         		: This software is provided "as is" without any 
+# 				   warranty; without even the implied warranty of 
+# 				    merchantability or fitness for a particular purpose. */
+
+package main
+import "fmt"
+
+// calculates factorial of a positive integer
+func fact(n int) int {
+	if n == 0 { // base cases: fact of 0
+		return 1
+	}
+	return n * fact(n-1)
+}
+
+func main() {
+	fmt.Println(fact(5))
+}

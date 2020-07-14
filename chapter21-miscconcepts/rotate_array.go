0e1283a09974072af8eda6fc9de53da8256d49c2
CareerMonk <careermonk@users.noreply.github.com>
2020-07-14 17:29:09 +0530

    Create rotate_array.go

---
+++ chapter21-miscconcepts/rotate_array.go
@@ -0,0 +1,54 @@
+// Copyright (c) July 12, 2020 CareerMonk Publications and others.
+// E-Mail           		: info@careermonk.com
+// Creation Date    		: 2020-07-12 06:15:46
+// Last modification		: 2020-07-12
+//               by		: Narasimha Karumanchi
+// Book Title			: Data Structures And Algorithmic Thinking With Go
+// Warranty         		: This software is provided "as is" without any
+// 				   warranty; without even the implied warranty of
+// 				    merchantability or fitness for a particular purpose.
+
+package main
+import "fmt"
+
+func leftRotate(arr []int, d int, n int) {
+	if d == 0 {
+		return
+	}
+	reverseArray(arr, 0, d-1)
+	reverseArray(arr, d, n-1)
+	reverseArray(arr, 0, n-1)
+}
+
+func printArray(arr []int, size int) {
+	var i int
+	//UTILITY FUNCTIONS
+	// function to print an array
+	for i = 0; i < size; i++ {
+		fmt.Printf("%d ", arr[i])
+	}
+}
+
+func reverseArray(arr []int, start int, end int) {
+	var temp int
+	for start < end {
+		//Function to reverse arr[] from index start to end
+		temp = arr[start]
+		arr[start] = arr[end]
+		arr[end] = temp
+		start++
+		end--
+	}
+}
+
+// Driver program to test above functions
+func main() {
+	var arr []int = []int{1, 2, 3, 4, 5, 6, 7}
+	var n int = len(arr)
+	var d int = 2
+	// in case the rotating factor is greater than array length
+	d = d % n
+	leftRotate(arr, d, n)
+	printArray(arr, n)
+	return
+}

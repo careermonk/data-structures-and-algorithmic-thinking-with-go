0313dbe163b736c2cfa36a5faf80641033584218
CareerMonk <careermonk@users.noreply.github.com>
2020-07-17 16:15:15 +0530

    Create remove_duplicates.go

---
+++ chapter03-linked-lists/remove_duplicates.go
@@ -0,0 +1,22 @@
+// Copyright (c) July 12, 2020 CareerMonk Publications and others.
+// E-Mail           		: info@careermonk.com
+// Creation Date    		: 2020-07-12 06:15:46
+// Last modification		: 2020-07-12
+//               by		: Narasimha Karumanchi
+// Book Title			: Data Structures And Algorithmic Thinking With Go
+// Warranty         		: This software is provided "as is" without any
+// 				   warranty; without even the implied warranty of
+// 				    merchantability or fitness for a particular purpose.
+
+func removeDuplicates(head *ListNode) *ListNode {
+    current := head
+    for current != nil {
+        if current.next != nil && current.data == current.next.data {
+            current.next = current.next.next
+            continue
+        }
+        current = current.next
+    }
+    
+    return head
+}

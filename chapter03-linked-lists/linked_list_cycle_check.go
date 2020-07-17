db9e3125dfa060933a9035322a99e27f15625c93
CareerMonk <careermonk@users.noreply.github.com>
2020-07-17 15:19:18 +0530

    Create linked_list_cycle_check.go

---
+++ chapter03-linked-lists/linked_list_cycle_check.go
@@ -0,0 +1,21 @@
+// Copyright (c) July 12, 2020 CareerMonk Publications and others.
+// E-Mail           		: info@careermonk.com
+// Creation Date    		: 2020-07-12 06:15:46
+// Last modification		: 2020-07-12
+//               by		: Narasimha Karumanchi
+// Book Title			: Data Structures And Algorithmic Thinking With Go
+// Warranty         		: This software is provided "as is" without any
+// 				   warranty; without even the implied warranty of
+// 				    merchantability or fitness for a particular purpose.
+
+func hasCycle(head *ListNode) bool {
+    fast, slow := head, head
+    for fast != nil && fast.next != nil {
+        fast = fast.next.next
+        slow = slow.next
+        if fast == slow {
+            return true
+        }
+    }
+    return false
+}

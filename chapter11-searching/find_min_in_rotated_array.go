075bb81318b95e0f12e0b466a9668dabdb6ab2ef
CareerMonk <careermonk@users.noreply.github.com>
2020-07-26 00:17:07 +0530

    Update find_min_in_rotated_array.go

--- chapter11-searching/find_min_in_rotated_array.go
+++ chapter11-searching/find_min_in_rotated_array.go
@@ -1,12 +1,12 @@
-# Copyright (c) July 12, 2020 CareerMonk Publications and others.
-# E-Mail           		: info@careermonk.com 
-# Creation Date    		: 2020-07-12 06:15:46 
-# Last modification		: 2020-07-12 
-#               by		: Narasimha Karumanchi 
-# Book Title			: Data Structures And Algorithmic Thinking With Go
-# Warranty         		: This software is provided "as is" without any 
-# 				   warranty; without even the implied warranty of 
-# 				    merchantability or fitness for a particular purpose.
+// Copyright (c) July 12, 2020 CareerMonk Publications and others.
+// E-Mail           		: info@careermonk.com
+// Creation Date    		: 2020-07-12 06:15:46
+// Last modification		: 2020-07-12
+//               by		: Narasimha Karumanchi
+// Book Title			: Data Structures And Algorithmic Thinking With Go
+// Warranty         		: This software is provided "as is" without any
+// 				   warranty without even the implied warranty of
+// 				    merchantability or fitness for a particular purpose.
 
 package main
 

b8dd8f79183dbaab11f2702af69abd843ee0a606
CareerMonk <careermonk@users.noreply.github.com>
2020-07-26 21:39:26 +0530

    Create is_palindrome_no_string_reverse.go

---
+++ chapter05-stacks/is_palindrome_no_string_reverse.go
@@ -0,0 +1,30 @@
+// Copyright (c) July 12, 2020 CareerMonk Publications and others.
+// E-Mail           		: info@careermonk.com
+// Creation Date    		: 2020-07-12 06:15:46
+// Last modification		: 2020-07-12
+//               by		: Narasimha Karumanchi
+// Book Title			: Data Structures And Algorithmic Thinking With Go
+// Warranty         		: This software is provided "as is" without any
+// 				   warranty without even the implied warranty of
+//
+
+package main
+
+import (
+	"fmt"
+)
+
+func isPalindrome(input string) bool {
+	for i := 0; i < len(input)/2; i++ {
+		if input[i] != input[len(input)-i-1] {
+			return false
+		}
+	}
+	return true
+}
+
+
+func main() {
+	fmt.Println(isPalindrome("abcedabcd"))
+	fmt.Println(isPalindrome("abccba"))
+}

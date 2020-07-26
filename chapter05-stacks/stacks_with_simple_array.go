6444f77cc5b27b751b539e94abf6759cd683283e
CareerMonk <careermonk@users.noreply.github.com>
2020-07-26 22:03:06 +0530

    Create stacks_with_simple_array.go

---
+++ chapter05-stacks/stacks_with_simple_array.go
@@ -0,0 +1,87 @@
+// Copyright (c) July 12, 2020 CareerMonk Publications and others.
+// E-Mail           		: info@careermonk.com
+// Creation Date    		: 2020-07-12 06:15:46
+// Last modification		: 2020-07-12
+//               by		: Narasimha Karumanchi
+// Book Title			: Data Structures And Algorithmic Thinking With Go
+// Warranty         		: This software is provided "as is" without any
+// 				   warranty without even the implied warranty of
+//
+
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Stack struct {
+	top      int
+	capacity uint
+	array    []interface{}
+}
+
+// Returns an initialized stack
+func (stack *Stack) Init(capacity uint) *Stack {
+	stack.top = -1
+	stack.capacity = capacity
+	stack.array = make([]interface{}, capacity)
+	return stack
+}
+
+// Returns an new stack
+func NewStack(capacity uint) *Stack {
+	return new(Stack).Init(capacity)
+}
+
+// Stack is full when top is equal to the last index
+func (stack *Stack) IsFull() bool {
+	return stack.top == int(stack.capacity)-1
+}
+
+// Stack is empty when top is equal to -1
+func (stack *Stack) IsEmpty() bool {
+	return stack.top == -1
+}
+
+func (stack *Stack) Push(data interface{}) error {
+	if stack.IsFull() {
+		return errors.New("stack is full")
+	}
+	stack.top++
+	stack.array[stack.top] = data
+	return nil
+}
+
+func (stack *Stack) Pop() (interface{}, error) {
+	if stack.IsEmpty() {
+		return nil, errors.New("stack is empty")
+	}
+	temp := stack.array[stack.top]
+	stack.top--
+	return temp, nil
+}
+
+func (stack *Stack) Peek() (interface{}, error) {
+	if stack.IsEmpty() {
+		return nil, errors.New("stack is empty")
+	}
+	temp := stack.array[stack.top]
+	return temp, nil
+}
+
+func main() {
+	stack := NewStack(100)
+
+	stack.Push(10)
+	fmt.Println("Pushed to stack : 10")
+	stack.Push(20)
+	fmt.Println("Pushed to stack : 20")
+	stack.Push(30)
+	fmt.Println("Pushed to stack : 30")
+
+	data, _ := stack.Peek()
+	fmt.Println("Top element : ", data)
+	data, _ = stack.Pop()
+	fmt.Println("Popped from stack : ", data)
+}

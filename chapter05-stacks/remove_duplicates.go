34433ae97aa75da6ee86061f40e7ba3d03136d08
CareerMonk <careermonk@users.noreply.github.com>
2020-07-27 15:15:34 +0530

    Create remove_duplicates.go

---
+++ chapter05-stacks/remove_duplicates.go
@@ -0,0 +1,32 @@
+// Copyright (c) July 12, 2020 CareerMonk Publications and others.
+// E-Mail           		: info@careermonk.com
+// Creation Date    		: 2020-07-12 06:15:46
+// Last modification		: 2020-07-12
+//               by		: Narasimha Karumanchi
+// Book Title			: Data Structures And Algorithmic Thinking With Go
+// Warranty         		: This software is provided "as is" without any
+// 				   warranty; without even the implied warranty of
+// 				    merchantability or fitness for a particular purpose.
+package main
+
+import (
+	"fmt"
+)
+
+func removeDuplicates(S string) string {
+	stack := make([]byte, 0, len(S))
+	for i := range S {
+		if len(stack) > 0 && stack[len(stack)-1] == S[i] {
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, S[i])
+		}
+	}
+	return string(stack)
+}
+
+func main() {
+	fmt.Println(removeDuplicates("abbaca"))
+	fmt.Println(removeDuplicates("careermonk"))
+	fmt.Println(removeDuplicates("mississippi"))
+}

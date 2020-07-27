431ccbd678e6fd01fe1351ea11a3e7f9443a4f84
CareerMonk <careermonk@users.noreply.github.com>
2020-07-27 18:57:43 +0530

    Create find_min.go

---
+++ chapter12-selection-algorithms/find_min.go
@@ -0,0 +1,32 @@
+// Copyright (c) July 12, 2020 CareerMonk Publications and others.
+// E-Mail           		: info@careermonk.com
+// Creation Date    		: 2020-07-12 06:15:46
+// Last modification		: 2020-07-12
+//               by		: Narasimha Karumanchi
+// Book Title			: Data Structures And Algorithmic Thinking With Go
+// Warranty         		: This software is provided "as is" without any
+// 				   warranty without even the implied warranty of
+//
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func findMin(A []int) (min int) {
+	min = math.MaxInt32
+	for _, value := range A {
+		if value < min {
+			min = value
+		}
+	}
+	return min
+}
+
+
+func main() {
+	var A = []int{11, -4, 7, 8, -10}
+	fmt.Println("Min: ", findMin(A))
+}

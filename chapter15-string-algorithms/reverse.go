79dcf357d242fe7b93c611101c906a01db24eca3
CareerMonk <careermonk@users.noreply.github.com>
2020-07-29 00:13:44 +0530

    Create reverse.go

---
+++ chapter15-string-algorithms/reverse.go
@@ -0,0 +1,38 @@
+// Copyright (c) July 12, 2020 CareerMonk Publications and others.
+// E-Mail           		: info@careermonk.com
+// Creation Date    		: 2020-07-12 06:15:46
+// Last modification		: 2020-07-12
+//               by		: Narasimha Karumanchi
+// Book Title			: Data Structures And Algorithmic Thinking With Go
+// Warranty         		: This software is provided "as is" without any
+// 				   warranty without even the implied warranty of
+//
+
+package main
+
+import (
+	"fmt"
+)
+
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+
+func Reverse2(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i] ^= r[j]
+		r[j] ^= r[i]
+		r[i] ^= r[j]
+	}
+	return string(r)
+}
+
+func main() {
+	fmt.Println(Reverse("Hello, world"))
+	fmt.Println(Reverse2("Hello, world"))
+}

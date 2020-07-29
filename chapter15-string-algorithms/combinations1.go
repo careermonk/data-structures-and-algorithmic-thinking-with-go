52548b9fac856763f039a8893deb34a49bf2e00d
CareerMonk <careermonk@users.noreply.github.com>
2020-07-29 18:04:35 +0530

    Create combinations1.go

---
+++ chapter15-string-algorithms/combinations1.go
@@ -0,0 +1,42 @@
+// Copyright (c) July 12, 2020 CareerMonk Publications and others.
+// E-Mail           		: info@careermonk.com
+// Creation Date    		: 2020-07-12 06:15:46
+// Last modification		: 2020-07-12
+//               by		: Narasimha Karumanchi
+// Book Title			: Data Structures And Algorithmic Thinking With Go
+// Warranty         		: This software is provided "as is" without any
+// 				   warranty without even the implied warranty of
+//
+
+package main
+
+import (
+	"fmt"
+)
+
+// combinations returns all combinations for a given string array.
+// This is essentially a powerset of the given set except that the empty set is disregarded.
+func combinations(set []string) (subsets [][]string) {
+	length := uint(len(set))
+
+	// Go through all possible combinations of objects
+	// from 1 (only first object in subset) to 2^length (all objects in subset)
+	for subsetBits := 1; subsetBits < (1 << length); subsetBits++ {
+		var subset []string
+
+		for object := uint(0); object < length; object++ {
+			// checks if object is contained in subset by checking if bit 'object' is set in subsetBits
+			if (subsetBits>>object)&1 == 1 {
+				// add object to subset
+				subset = append(subset, set[object])
+			}
+		}
+		// add subset to subsets
+		subsets = append(subsets, subset)
+	}
+	return subsets
+}
+
+func main() {
+	fmt.Println(combinations([]string{"a", "b", "c"}))
+}

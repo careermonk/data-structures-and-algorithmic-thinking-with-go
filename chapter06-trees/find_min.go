21699482ac14c4c870946f88f98a1eda748af691
CareerMonk <careermonk@users.noreply.github.com>
2020-07-31 14:56:36 +0530

    Create find_min.go

---
+++ chapter06-trees/find_min.go
@@ -0,0 +1,71 @@
+// Copyright (c) July 12, 2020 CareerMonk Publications and others.
+// E-Mail           		: info@careermonk.com
+// Creation Date    		: 2020-07-12 06:15:46
+// Last modification		: 2020-07-12
+//               by		: Narasimha Karumanchi
+// Book Title			: Data Structures And Algorithmic Thinking With Go
+// Warranty         		: This software is provided "as is" without any
+// 				   warranty without even the implied warranty of
+//
+
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+// A BinraryTreeNode is a binary tree with integer values.
+type BinraryTreeNode struct {
+	left  *BinraryTreeNode
+	data  int
+	right *BinraryTreeNode
+}
+
+// NewBinraryTree returns a new, random binary tree holding the values 1k, 2k, ..., nk.
+func NewBinraryTree(n, k int) *BinraryTreeNode {
+	var root *BinraryTreeNode
+	for _, v := range rand.Perm(n) {
+		root = insert(root, (1+v)*k)
+	}
+	return root
+}
+
+func insert(root *BinraryTreeNode, v int) *BinraryTreeNode {
+	if root == nil {
+		return &BinraryTreeNode{nil, v, nil}
+	}
+	if v < root.data {
+		root.left = insert(root.left, v)
+		return root
+	}
+	root.right = insert(root.right, v)
+	return root
+}
+
+func findMin(root *BinraryTreeNode) int {
+	min := math.MaxInt32
+	if root != nil {
+		root_val := root.data
+		left := findMin(root.left)
+		right := findMin(root.right)
+
+		// Find the largest of the three values.
+		if left < right {
+			min = left
+		} else {
+			min = right
+		}
+
+		if root_val < min {
+			min = root_val
+		}
+	}
+	return min
+}
+
+func main() {
+	t1 := NewBinraryTree(10, 1)
+	fmt.Println(findMin(t1))
+}

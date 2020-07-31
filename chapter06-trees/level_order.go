84143d342c97350295c64257bc7d3b87920558ff
CareerMonk <careermonk@users.noreply.github.com>
2020-07-31 14:47:21 +0530

    Create level_order.go

---
+++ chapter06-trees/level_order.go
@@ -0,0 +1,75 @@
+// Copyright (c) July 12, 2020 CareerMonk Publications and others.
+// E-Mail           		: info@careermonk.com
+// Creation Date    		: 2020-07-12 06:15:46
+// Last modification		: 2020-07-12
+//               by		: Narasimha Karumanchi
+// Book Title			: Data Structures And Algorithmic Thinking With Go
+// Warranty         		: This software is provided "as is" without any
+// 				   warranty without even the implied warranty of
+//
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+// A BinraryTreeNode is a binary tree with integer values.
+type BinraryTreeNode struct {
+	left  *BinraryTreeNode
+	data  int
+	right *BinraryTreeNode
+}
+
+// NewBinraryTree returns a new, random binary tree holding the values 1k, 2k, ..., nk.
+func NewBinraryTree(n, k int) *BinraryTreeNode {
+	var root *BinraryTreeNode
+	for _, v := range rand.Perm(n) {
+		root = insert(root, (1+v)*k)
+	}
+	return root
+}
+
+func insert(root *BinraryTreeNode, v int) *BinraryTreeNode {
+	if root == nil {
+		return &BinraryTreeNode{nil, v, nil}
+	}
+	if v < root.data {
+		root.left = insert(root.left, v)
+		return root
+	}
+	root.right = insert(root.right, v)
+	return root
+}
+
+func levelOrder(root *BinraryTreeNode) [][]int { // Data from each level is being returned as a separate list
+	if root == nil {
+		return [][]int{}
+	}
+	var result [][]int
+	queue := []*BinraryTreeNode{root}
+	for len(queue) > 0 {
+		qlen := len(queue)
+		var level []int
+		for i := 0; i < qlen; i++ {
+			node := queue[0]
+			level = append(level, node.data)
+			queue = queue[1:]
+
+			if node.left != nil {
+				queue = append(queue, node.left)
+			}
+			if node.right != nil {
+				queue = append(queue, node.right)
+			}
+		}
+		result = append(result, level)
+	}
+	return res
+}
+
+func main() {
+	t1 := NewBinraryTree(10, 1)
+	fmt.Println(levelOrder(t1))
+}

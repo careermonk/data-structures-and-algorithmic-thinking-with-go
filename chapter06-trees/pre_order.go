e085247b68965c87e0ac853d7ef161ab3538ac82
CareerMonk <careermonk@users.noreply.github.com>
2020-07-31 17:38:32 +0530

    Update pre_order.go

--- chapter06-trees/pre_order.go
+++ chapter06-trees/pre_order.go
@@ -15,15 +15,15 @@ import (
 	"math/rand"
 )
 
-// A BinraryTreeNode is a binary tree with integer values.
-type BinraryTreeNode struct {
-	left  *BinraryTreeNode
+// A BinaryTreeNode is a binary tree with integer values.
+type BinaryTreeNode struct {
+	left  *BinaryTreeNode
 	data  int
-	right *BinraryTreeNode
+	right *BinaryTreeNode
 }
 
 // PreOrder traverses a tree in pre-order
-func PreOrder(root *BinraryTreeNode) {
+func PreOrder(root *BinaryTreeNode) {
 	if root == nil {
 		return
 	}
@@ -33,7 +33,7 @@ func PreOrder(root *BinraryTreeNode) {
 }
 
 // PreOrder traverses a tree in pre-order, sending each data on a channel.
-func PreOrderWalk(root *BinraryTreeNode, ch chan int) {
+func PreOrderWalk(root *BinaryTreeNode, ch chan int) {
 	if root == nil {
 		return
 	}
@@ -43,7 +43,7 @@ func PreOrderWalk(root *BinraryTreeNode, ch chan int) {
 }
 
 // Walker launches Walk in a new goroutine, and returns a read-only channel of values.
-func PreOrderWalker(root *BinraryTreeNode) <-chan int {
+func PreOrderWalker(root *BinaryTreeNode) <-chan int {
 	ch := make(chan int)
 	go func() {
 		PreOrderWalk(root, ch)
@@ -52,18 +52,18 @@ func PreOrderWalker(root *BinraryTreeNode) <-chan int {
 	return ch
 }
 
-// NewBinraryTree returns a new, random binary tree holding the values 1k, 2k, ..., nk.
-func NewBinraryTree(n, k int) *BinraryTreeNode {
-	var root *BinraryTreeNode
+// NewBinaryTree returns a new, random binary tree holding the values 1k, 2k, ..., nk.
+func NewBinaryTree(n, k int) *BinaryTreeNode {
+	var root *BinaryTreeNode
 	for _, v := range rand.Perm(n) {
 		root = insert(root, (1+v)*k)
 	}
 	return root
 }
 
-func insert(root *BinraryTreeNode, v int) *BinraryTreeNode {
+func insert(root *BinaryTreeNode, v int) *BinaryTreeNode {
 	if root == nil {
-		return &BinraryTreeNode{nil, v, nil}
+		return &BinaryTreeNode{nil, v, nil}
 	}
 	if v < root.data {
 		root.left = insert(root.left, v)
@@ -74,7 +74,7 @@ func insert(root *BinraryTreeNode, v int) *BinraryTreeNode {
 }
 
 func main() {
-	t1 := NewBinraryTree(10, 1)
+	t1 := NewBinaryTree(10, 1)
 
 	// Pre-Order walk with print statements
 	PreOrder(t1)

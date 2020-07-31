807ab47b9eb3e287c9800f29349b974dfaaf6f08
CareerMonk <careermonk@users.noreply.github.com>
2020-07-31 14:30:36 +0530

    Create post_order.go

---
+++ chapter06-trees/post_order.go
@@ -0,0 +1,94 @@
+// Copyright (c) July 12, 2020 CareerMonk Publications and others.
+// E-Mail           		: info@careermonk.com
+// Creation Date    		: 2020-07-12 06:15:46
+// Last modification		: 2020-07-12
+//               by		: Narasimha Karumanchi
+// Book Title			: Data Structures And Algorithmic Thinking With Go
+// Warranty         		: This software is provided "as is" without any
+// 				   warranty without even the implied warranty of
+//
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+// A BinraryTreeNode is a binary tree with integer values.
+type BinraryTreeNode struct {
+	left  *BinraryTreeNode
+	data  int
+	right *BinraryTreeNode
+}
+
+// PostOrder traverses a tree in pre-order
+func PostOrder(root *BinraryTreeNode) {
+	if root == nil {
+		return
+	}
+
+	PostOrder(root.left)
+	PostOrder(root.right)
+	fmt.Printf("%d ", root.data)
+}
+
+// PostOrderWalk traverses a tree in in-order, sending each data on a channel.
+func PostOrderWalk(root *BinraryTreeNode, ch chan int) {
+	if root == nil {
+		return
+	}
+
+	PostOrderWalk(root.left, ch)
+	PostOrderWalk(root.right, ch)
+	ch <- root.data
+}
+
+// PostOrderWalker launches Walk in a new goroutine, and returns a read-only channel of values.
+func PostOrderWalker(root *BinraryTreeNode) <-chan int {
+	ch := make(chan int)
+	go func() {
+		PostOrderWalk(root, ch)
+		close(ch)
+	}()
+	return ch
+}
+
+// NewBinraryTree returns a new, random binary tree holding the values 1k, 2k, ..., nk.
+func NewBinraryTree(n, k int) *BinraryTreeNode {
+	var root *BinraryTreeNode
+	for _, v := range rand.Perm(n) {
+		root = insert(root, (1+v)*k)
+	}
+	return root
+}
+
+func insert(root *BinraryTreeNode, v int) *BinraryTreeNode {
+	if root == nil {
+		return &BinraryTreeNode{nil, v, nil}
+	}
+	if v < root.data {
+		root.left = insert(root.left, v)
+		return root
+	}
+	root.right = insert(root.right, v)
+	return root
+}
+
+func main() {
+	t1 := NewBinraryTree(10, 1)
+
+	// PostOrder walk with print statements
+	PostOrder(t1)
+	fmt.Println()
+
+	// PostOrderWalker walk with channel
+	c := PostOrderWalker(t1)
+	for {
+		v, ok := <-c
+		if !ok {
+			break
+		}
+		fmt.Printf("%d ", v)
+	}
+}

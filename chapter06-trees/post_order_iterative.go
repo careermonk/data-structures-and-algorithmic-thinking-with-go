8fd186b39fc166a02d6ce5917db00b8a0357b2f6
CareerMonk <careermonk@users.noreply.github.com>
2020-07-31 14:30:28 +0530

    Create post_order_iterative.go

---
+++ chapter06-trees/post_order_iterative.go
@@ -0,0 +1,220 @@
+// Copyright (c) July 12, 2020 CareerMonk Publications and others.
+// E-Mail           		: info@careermonk.com
+// Creation Date    		: 2020-07-12 06:15:46
+// Last modification		: 2020-07-12
+//               by		: Narasimha Karumanchi
+// Book Title			: Data Structures And Algorithmic Thinking With Go
+// Warranty         		: This software is provided "as is" without any
+// 				   warranty without even the implied warranty of
+//
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+// A BinraryTreeNode is a binary tree with integer values.
+type BinraryTreeNode struct {
+	left  *BinraryTreeNode
+	data  int
+	right *BinraryTreeNode
+}
+
+// NewBinraryTree returns a new, random binary tree holding the values 1k, 2k, ..., nk.
+func NewBinraryTree(n, k int) *BinraryTreeNode {
+	var root *BinraryTreeNode
+	for _, v := range rand.Perm(n) {
+		root = insert(root, (1+v)*k)
+	}
+	return root
+}
+
+func insert(root *BinraryTreeNode, v int) *BinraryTreeNode {
+	if root == nil {
+		return &BinraryTreeNode{nil, v, nil}
+	}
+	if v < root.data {
+		root.left = insert(root.left, v)
+		return root
+	}
+	root.right = insert(root.right, v)
+	return root
+}
+
+type Stack struct {
+	top      int
+	capacity uint
+	array    []interface{}
+}
+
+// Returns an initialized stack
+func (stack *Stack) Init(capacity uint) *Stack {
+	stack.top = -1
+	stack.capacity = capacity
+	stack.array = make([]interface{}, capacity)
+	return stack
+}
+
+// Returns an new stack
+func NewStack(capacity uint) *Stack {
+	return new(Stack).Init(capacity)
+}
+
+// Returns the size of the size
+func (stack *Stack) Size() uint {
+	return uint(stack.top + 1)
+}
+
+// Stack is full when top is equal to the last index
+func (stack *Stack) IsFull() bool {
+	return stack.top == int(stack.capacity)-1
+}
+
+// Stack is empty when top is equal to -1
+func (stack *Stack) IsEmpty() bool {
+	return stack.top == -1
+}
+
+func (stack *Stack) Resize() {
+	if stack.IsFull() {
+		stack.capacity *= 2
+	} else {
+		stack.capacity /= 2
+	}
+
+	target := make([]interface{}, stack.capacity)
+	copy(target, stack.array[:stack.top+1])
+	stack.array = target
+}
+
+func (stack *Stack) Push(data interface{}) error {
+	if stack.IsFull() {
+		stack.Resize()
+	}
+	stack.top++
+	stack.array[stack.top] = data
+	return nil
+}
+
+func (stack *Stack) Pop() interface{} {
+	var i interface{}
+	if stack.IsEmpty() {
+		return i
+	}
+	temp := stack.array[stack.top]
+	stack.top--
+	if stack.Size() < stack.capacity/2 {
+		stack.Resize()
+	}
+	return temp
+}
+
+func (stack *Stack) Peek() interface{} {
+	var i interface{}
+	if stack.IsEmpty() {
+		return i
+	}
+	temp := stack.array[stack.top]
+	return temp
+}
+
+// Drain removes all elements that are currently in the stack.
+func (stack *Stack) Drain() {
+	stack.array = nil
+	stack.top = -1
+}
+
+// PostOrderWalk traverses a tree in in-order, sending each data on a channel.
+func PostOrderWalk(root *BinraryTreeNode, ch chan int) {
+	if root == nil {
+		return
+	}
+
+	PostOrderWalk(root.left, ch)
+	PostOrderWalk(root.right, ch)
+	ch <- root.data
+}
+
+// PostOrderWalker launches Walk in a new goroutine, and returns a read-only channel of values.
+func PostOrderWalker(root *BinraryTreeNode) <-chan int {
+	ch := make(chan int)
+	go func() {
+		PostOrderWalk(root, ch)
+		close(ch)
+	}()
+	return ch
+}
+
+func PostOrder1(root *BinraryTreeNode) []int {
+	var result []int
+	stack := NewStack(1)
+	stack.Push(root)
+	for !stack.IsEmpty() {
+		temp := stack.Pop().(*BinraryTreeNode)
+
+		result = append(result, temp.data)
+
+		if temp.left != nil {
+			stack.Push(temp.left)
+		}
+
+		if temp.right != nil {
+			stack.Push(temp.right)
+		}
+	}
+	n := len(result)
+	for i := 0; i < n/2; i++ {
+		j := n - i - 1
+		result[i], result[j] = result[j], result[i]
+	}
+	fmt.Println(result)
+	return result
+}
+
+func PostOrder2(root *BinraryTreeNode) []int {
+	result := []int{}
+	stack := []*BinraryTreeNode{root}
+
+	for len(stack) > 0 {
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if root == nil {
+			continue
+		}
+
+		result = append(result, root.data)
+
+		stack = append(stack, root.left)
+		stack = append(stack, root.right)
+
+	}
+	// reverse
+	n := len(result)
+	for i := 0; i < n/2; i++ {
+		j := n - i - 1
+		result[i], result[j] = result[j], result[i]
+	}
+	fmt.Println(result)
+	return result
+}
+
+func main() {
+	t1 := NewBinraryTree(10, 1)
+	PostOrder1(t1)
+	fmt.Println()
+	PostOrder2(t1)
+	fmt.Println()
+
+	// PostOrderWalker walk with channel
+	c := PostOrderWalker(t1)
+	for {
+		v, ok := <-c
+		if !ok {
+			break
+		}
+		fmt.Printf("%d ", v)
+	}
+}

64631f08349a0938ad63eb5f84f5cc57c2db1d87
CareerMonk <careermonk@users.noreply.github.com>
2020-07-31 16:20:28 +0530

    Create level_order_bottom_up.go

---
+++ chapter06-trees/level_order_bottom_up.go
@@ -0,0 +1,178 @@
+// Copyright (c) July 12, 2020 CareerMonk Publications and others.
+// E-Mail           		: info@careermonk.com
+// Creation Date    		: 2020-07-12 06:15:46
+// Last modification		: 2020-07-12
+//               by		: Narasimha Karumanchi
+// Book Title			: Data Structures And Algorithmic Thinking With Go
+// Warranty         		: This software is provided "as is" without any
+// 				   warranty without even the implied warranty of
+//
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+// A BinraryTreeNode is a binary tree with integer values.
+type BinraryTreeNode struct {
+	left  *BinraryTreeNode
+	data  int
+	right *BinraryTreeNode
+}
+
+// NewBinraryTree returns a new, random binary tree holding the values 1k, 2k, ..., nk.
+func NewBinraryTree(n, k int) *BinraryTreeNode {
+	var root *BinraryTreeNode
+	for _, v := range rand.Perm(n) {
+		root = Insert(root, (1+v)*k)
+	}
+	return root
+}
+
+func Insert(root *BinraryTreeNode, v int) *BinraryTreeNode {
+	newNode := &BinraryTreeNode{nil, v, nil}
+	if root == nil {
+		return newNode
+	}
+	queue := []*BinraryTreeNode{root}
+	for len(queue) > 0 {
+		qlen := len(queue)
+		for i := 0; i < qlen; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			if node.left != nil {
+				queue = append(queue, node.left)
+			} else {
+				node.left = newNode
+				return root
+			}
+			if node.right != nil {
+				queue = append(queue, node.right)
+			} else {
+				node.right = newNode
+				return root
+			}
+		}
+	}
+	return root
+}
+
+func LevelOrderBottomUp(root *BinraryTreeNode) [][]int { // Data from each level is being returned as a separate list
+	if root == nil {
+		return [][]int{}
+	}
+	var result [][]int
+	queue := []*BinraryTreeNode{root}
+	stack := NewStack(1)
+
+	for len(queue) > 0 {
+		qlen := len(queue)
+		var level []int
+		for i := 0; i < qlen; i++ {
+			node := queue[0]
+			level = append(level, node.data)
+			queue = queue[1:]
+			if node.right != nil {
+				queue = append(queue, node.right)
+			}
+			if node.left != nil {
+				queue = append(queue, node.left)
+			}
+
+		}
+		stack.Push(level)
+	}
+	for !stack.IsEmpty() {
+		result = append(result, stack.Pop().([]int))
+	}
+	return result
+}
+
+func main() {
+	t1 := NewBinraryTree(20, 1)
+	fmt.Println(LevelOrderBottomUp(t1))
+}
+
+type Stack struct {
+	top      int
+	capacity uint
+	array    []interface{}
+}
+
+// Returns an initialized stack
+func (stack *Stack) Init(capacity uint) *Stack {
+	stack.top = -1
+	stack.capacity = capacity
+	stack.array = make([]interface{}, capacity)
+	return stack
+}
+
+// Returns an new stack
+func NewStack(capacity uint) *Stack {
+	return new(Stack).Init(capacity)
+}
+
+// Returns the size of the size
+func (stack *Stack) Size() uint {
+	return uint(stack.top + 1)
+}
+
+// Stack is full when top is equal to the last index
+func (stack *Stack) IsFull() bool {
+	return stack.top == int(stack.capacity)-1
+}
+
+// Stack is empty when top is equal to -1
+func (stack *Stack) IsEmpty() bool {
+	return stack.top == -1
+}
+
+func (stack *Stack) Resize() {
+	if stack.IsFull() {
+		stack.capacity *= 2
+	} else {
+		stack.capacity /= 2
+	}
+	target := make([]interface{}, stack.capacity)
+	copy(target, stack.array[:stack.top+1])
+	stack.array = target
+}
+
+func (stack *Stack) Push(data interface{}) error {
+	if stack.IsFull() {
+		stack.Resize()
+	}
+	stack.top++
+	stack.array[stack.top] = data
+	return nil
+}
+
+func (stack *Stack) Pop() interface{} {
+	var i interface{}
+	if stack.IsEmpty() {
+		return i
+	}
+	temp := stack.array[stack.top]
+	stack.top--
+	if stack.Size() < stack.capacity/2 {
+		stack.Resize()
+	}
+	return temp
+}
+
+func (stack *Stack) Peek() interface{} {
+	var i interface{}
+	if stack.IsEmpty() {
+		return i
+	}
+	temp := stack.array[stack.top]
+	return temp
+}
+
+// Drain removes all elements that are currently in the stack.
+func (stack *Stack) Drain() {
+	stack.array = nil
+	stack.top = -1
+}

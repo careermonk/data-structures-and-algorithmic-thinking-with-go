c832fca122f9b807d5e8951d040fe1fa75d51856
CareerMonk <careermonk@users.noreply.github.com>
2020-07-31 18:49:32 +0530

    Create check_path_sum.go

---
+++ chapter06-trees/check_path_sum.go
@@ -0,0 +1,193 @@
+// Copyright (c) July 12, 2020 CareerMonk Publications and others.
+// E-Mail           		: info@careermonk.com
+// Creation Date    		: 2020-07-12 06:15:46
+// Last modification		: 2020-07-12
+//               by		: Narasimha Karumanchi
+// Book Title			: Data Structures And Algorithmic Thinking With Go
+// Warranty         		: This software is provided "as is" without any
+// 				   warranty without even the implied warranty of
+//
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+)
+
+// A BinaryTreeNode is a binary tree with integer values.
+type BinaryTreeNode struct {
+	left  *BinaryTreeNode
+	data  int
+	right *BinaryTreeNode
+}
+
+// NewBinaryTree returns a new, random binary tree holding the values 1k, 2k, ..., nk.
+func NewBinaryTree(n, k int) *BinaryTreeNode {
+	var root *BinaryTreeNode
+	for _, v := range rand.Perm(n) {
+		root = Insert(root, (1+v)*k)
+	}
+	return root
+}
+
+func Insert(root *BinaryTreeNode, v int) *BinaryTreeNode {
+	newNode := &BinaryTreeNode{nil, v, nil}
+	if root == nil {
+		return newNode
+	}
+	queue := []*BinaryTreeNode{root}
+	for len(queue) > 0 {
+		qlen := len(queue)
+		for i := 0; i < qlen; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			if node.left != nil {
+				queue = append(queue, node.left)
+			} else {
+				node.left = newNode
+				return root
+			}
+			if node.right != nil {
+				queue = append(queue, node.right)
+			} else {
+				node.right = newNode
+				return root
+			}
+		}
+	}
+	return root
+}
+
+// Compute the number of nodes in a tree.
+func Size(root *BinaryTreeNode) int {
+	if root == nil {
+		return 0
+	}
+	var result int
+	queue := []*BinaryTreeNode{root}
+	for len(queue) > 0 {
+		qlen := len(queue)
+		var level []int
+		for i := 0; i < qlen; i++ {
+			node := queue[0]
+			result++
+			level = append(level, node.data)
+			queue = queue[1:]
+			if node.left != nil {
+				queue = append(queue, node.left)
+			}
+			if node.right != nil {
+				queue = append(queue, node.right)
+			}
+		}
+
+	}
+	return result
+}
+
+func LevelOrder(root *BinaryTreeNode) [][]int { // Data from each level is being returned as a separate list
+	if root == nil {
+		return [][]int{}
+	}
+	var result [][]int
+	queue := []*BinaryTreeNode{root}
+	for len(queue) > 0 {
+		qlen := len(queue)
+		var level []int
+		for i := 0; i < qlen; i++ {
+			node := queue[0]
+			level = append(level, node.data)
+			queue = queue[1:]
+
+			if node.left != nil {
+				queue = append(queue, node.left)
+			}
+			if node.right != nil {
+				queue = append(queue, node.right)
+			}
+		}
+		result = append(result, level)
+	}
+	return result
+}
+
+func DeleteTree(root *BinaryTreeNode) *BinaryTreeNode {
+	if root == nil {
+		return nil
+	}
+	// first delete both subtrees
+	root.left = DeleteTree(root.left)
+	root.right = DeleteTree(root.right)
+	// Delete current node only after deleting subtrees
+	root = nil
+	return root
+}
+
+func BinaryTreePaths(root *BinaryTreeNode) []string {
+	result := make([]string, 0)
+
+	paths(root, "", &result)
+
+	return result
+}
+
+func paths(root *BinaryTreeNode, prefix string, result *[]string) {
+	if root == nil {
+		return
+	}
+	// append this node to the path array
+	if len(prefix) == 0 {
+		prefix += strconv.Itoa(root.data)
+	} else {
+		prefix += "->" + strconv.Itoa(root.data)
+	}
+
+	if root.left == nil && root.right == nil { // it's a leaf, so print the path that led to here
+		*result = append(*result, prefix+"\n")
+		return
+	}
+	// otherwise try both subtrees
+	paths(root.left, prefix, result)
+	paths(root.right, prefix, result)
+}
+
+func HasPathSum(root *BinaryTreeNode, sum int) bool {
+	allSums := make([]int, 0)
+	getAllSums(root, &allSums, 0)
+	for _, val := range allSums {
+		if sum == val {
+			allSums = []int{}
+			return true
+		}
+	}
+
+	allSums = []int{}
+	return false
+}
+
+func getAllSums(root *BinaryTreeNode, allSums *[]int, currSum int) {
+	if root != nil {
+		currSum += root.data
+		if root.left == nil && root.right == nil {
+			*allSums = append(*allSums, currSum)
+		} else {
+			getAllSums(root.left, allSums, currSum)
+			getAllSums(root.right, allSums, currSum)
+		}
+	}
+}
+
+func main() {
+	t1 := NewBinaryTree(10, 1)
+	fmt.Println(LevelOrder(t1))
+	fmt.Println(BinaryTreePaths(t1))
+
+	fmt.Println(HasPathSum(t1, 12))
+	fmt.Println(HasPathSum(t1, 14))
+	fmt.Println(HasPathSum(t1, 17))
+	fmt.Println(HasPathSum(t1, 30))
+
+	t1 = DeleteTree(t1)
+}

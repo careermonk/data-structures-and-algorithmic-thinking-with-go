dece9c44be1e8ef115a12427be9ee8a667683499
CareerMonk <careermonk@users.noreply.github.com>
2020-07-31 23:04:29 +0530

    Create all_ancestors.go

---
+++ chapter06-trees/all_ancestors.go
@@ -0,0 +1,228 @@
+// Copyright (c) July 12, 2020 CareerMonk Publications and others.
+// E-Mail           		: info@careermonk.com
+// Creation Date    		: 2020-07-12 06:15:46
+// Last modification		: 2020-07-12
+//               by		: Narasimha Karumanchi
+// Book Title			: Data Structures And Algorithmic Thinking With Go
+// Warranty         		: This software is provided "as is" without any
+// 				   warranty without even the implied warranty of
+//
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+)
+
+// A BinaryTreeNode is a binary tree with integer values.
+type BinaryTreeNode struct {
+	left  *BinaryTreeNode
+	data  int
+	right *BinaryTreeNode
+}
+
+// NewBinaryTree returns a new, random binary tree holding the values 1k, 2k, ..., nk.
+func NewBinaryTree(n, k int) *BinaryTreeNode {
+	var root *BinaryTreeNode
+	for _, v := range rand.Perm(n) {
+		root = Insert(root, (1+v)*k)
+	}
+	return root
+}
+
+func Insert(root *BinaryTreeNode, v int) *BinaryTreeNode {
+	newNode := &BinaryTreeNode{nil, v, nil}
+	if root == nil {
+		return newNode
+	}
+	queue := []*BinaryTreeNode{root}
+	for len(queue) > 0 {
+		qlen := len(queue)
+		for i := 0; i < qlen; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			if node.left != nil {
+				queue = append(queue, node.left)
+			} else {
+				node.left = newNode
+				return root
+			}
+			if node.right != nil {
+				queue = append(queue, node.right)
+			} else {
+				node.right = newNode
+				return root
+			}
+		}
+	}
+	return root
+}
+
+// Compute the number of nodes in a tree.
+func Size(root *BinaryTreeNode) int {
+	if root == nil {
+		return 0
+	}
+	var result int
+	queue := []*BinaryTreeNode{root}
+	for len(queue) > 0 {
+		qlen := len(queue)
+		var level []int
+		for i := 0; i < qlen; i++ {
+			node := queue[0]
+			result++
+			level = append(level, node.data)
+			queue = queue[1:]
+			if node.left != nil {
+				queue = append(queue, node.left)
+			}
+			if node.right != nil {
+				queue = append(queue, node.right)
+			}
+		}
+
+	}
+	return result
+}
+
+func LevelOrder(root *BinaryTreeNode) [][]int { // Data from each level is being returned as a separate list
+	if root == nil {
+		return [][]int{}
+	}
+	var result [][]int
+	queue := []*BinaryTreeNode{root}
+	for len(queue) > 0 {
+		qlen := len(queue)
+		var level []int
+		for i := 0; i < qlen; i++ {
+			node := queue[0]
+			level = append(level, node.data)
+			queue = queue[1:]
+
+			if node.left != nil {
+				queue = append(queue, node.left)
+			}
+			if node.right != nil {
+				queue = append(queue, node.right)
+			}
+		}
+		result = append(result, level)
+	}
+	return result
+}
+
+func DeleteTree(root *BinaryTreeNode) *BinaryTreeNode {
+	if root == nil {
+		return nil
+	}
+	// first delete both subtrees
+	root.left = DeleteTree(root.left)
+	root.right = DeleteTree(root.right)
+	// Delete current node only after deleting subtrees
+	root = nil
+	return root
+}
+
+// PreOrder traverses a tree in pre-order
+func PreOrder(root *BinaryTreeNode) {
+	if root == nil {
+		return
+	}
+	fmt.Printf("%d ", root.data)
+	PreOrder(root.left)
+	PreOrder(root.right)
+}
+
+// InOrder traverses a tree in pre-order
+func InOrder(root *BinaryTreeNode) {
+	if root == nil {
+		return
+	}
+
+	InOrder(root.left)
+	fmt.Printf("%d ", root.data)
+	InOrder(root.right)
+}
+
+// PostOrder traverses a tree in pre-order
+func PostOrder(root *BinaryTreeNode) {
+	if root == nil {
+		return
+	}
+
+	PostOrder(root.left)
+	PostOrder(root.right)
+	fmt.Printf("%d ", root.data)
+}
+
+func BinaryTreePaths(root *BinaryTreeNode) []string {
+	result := make([]string, 0)
+
+	paths(root, "", &result)
+
+	return result
+}
+
+func paths(root *BinaryTreeNode, prefix string, result *[]string) {
+	if root == nil {
+		return
+	}
+	// append this node to the path array
+	if len(prefix) == 0 {
+		prefix += strconv.Itoa(root.data)
+	} else {
+		prefix += "->" + strconv.Itoa(root.data)
+	}
+
+	if root.left == nil && root.right == nil { // it's a leaf, so print the path that led to here
+		*result = append(*result, prefix+"\n")
+		return
+	}
+	// otherwise try both subtrees
+	paths(root.left, prefix, result)
+	paths(root.right, prefix, result)
+}
+
+// Recursive function to print all ancestors of a given node in a binary tree. The
+// function returns true if the node is found in the subtree rooted at the given root node
+func printAncestors(root *BinaryTreeNode, node int) bool {
+	// base case
+	if root == nil {
+		return false
+	}
+
+	// return true if given node is found
+	if root.data == node {
+		return true
+	}
+
+	// search node in left subtree
+	left := printAncestors(root.left, node)
+
+	// search node in right subtree
+	right := false
+	if !left {
+		right = printAncestors(root.right, node)
+	}
+
+	// if given node is found in either left or right subtree,
+	// current node is an ancestor of given node
+	if left || right {
+		fmt.Printf("%d ", root.data)
+	}
+
+	// return true if node is found
+	return left || right
+}
+
+func main() {
+	t1 := NewBinaryTree(10, 1)
+	fmt.Println(BinaryTreePaths(t1))
+
+	printAncestors(t1, 4)
+	
+
+	t1 = DeleteTree(t1)
+}

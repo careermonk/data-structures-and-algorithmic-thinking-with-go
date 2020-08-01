bcceb05f23f7385b7684650e2de405962b77f741
CareerMonk <careermonk@users.noreply.github.com>
2020-08-02 00:57:26 +0530

    Create unival_tree.go

---
+++ chapter06-trees/unival_tree.go
@@ -0,0 +1,77 @@
+// Copyright (c) July 12, 2020 CareerMonk Publications and others.
+// E-Mail           		: info@careermonk.com
+// Creation Date    		: 2020-07-12 06:15:46
+// Last modification		: 2020-07-12
+//               by		: Narasimha Karumanchi
+// Book Title			: Data Structures And Algorithmic Thinking With Go
+// Warranty         		: This software is provided "as is" without any
+// 				   warranty without even the implied warranty of
+//
+
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+// A BinaryTreeNode is a binary tree with integer values.
+type BinaryTreeNode struct {
+	left  *BinaryTreeNode
+	data  int
+	right *BinaryTreeNode
+}
+
+// NewBinaryTree returns a new, random binary tree holding the values 1k, 2k, ..., nk.
+func NewBinaryTree(n, k int) *BinaryTreeNode {
+	var root *BinaryTreeNode
+	for _, v := range rand.Perm(n) {
+		root = insert(root, (1+v)*k)
+	}
+	return root
+}
+
+func insert(root *BinaryTreeNode, v int) *BinaryTreeNode {
+	if root == nil {
+		return &BinaryTreeNode{nil, v, nil}
+	}
+	if v < root.data {
+		root.left = insert(root.left, v)
+		return root
+	}
+	root.right = insert(root.right, v)
+	return root
+}
+
+func findMax(root *BinaryTreeNode) int {
+	max := math.MinInt32
+	if root != nil {
+		root_val := root.data
+		left := findMax(root.left)
+		right := findMax(root.right)
+
+		// Find the largest of the three values.
+		if left > right {
+			max = left
+		} else {
+			max = right
+		}
+
+		if root_val > max {
+			max = root_val
+		}
+	}
+	return max
+}
+
+func IsUnivalTree(root *BinaryTreeNode) bool {
+	isLeftUnivalTree := (root.left == nil || (root.data == root.left.data && IsUnivalTree(root.left)))
+	isRightUnivalTree := (root.right == nil || (root.data == root.right.data && IsUnivalTree(root.right)))
+	return isLeftUnivalTree && isRightUnivalTree
+}
+
+func main() {
+	t1 := NewBinaryTree(10, 1)
+	fmt.Println(IsUnivalTree(t1))
+}
